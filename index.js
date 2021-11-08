ec1ea6443f70a773a9d0ec81a3ff3e117e2ee42f
Sumit Yadav <93591986+syadav-deqode@users.noreply.github.com>
2021-11-08 10:58:31 +0000

    dummy commit

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 
 const app = express()
 
-const port = process.env.port || 5500
+const port = process.env.port || 5500;
 
 app.listen(port,()=>console.log(`App started at the port ${port}`))
